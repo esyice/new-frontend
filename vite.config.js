4b623d440f366f631a246780c53b74831017b443
eZice <144651926+esyice@users.noreply.github.com>
2025-03-13 08:43:18 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   server:{
     proxy:{
-      '/api':'http://localhost:3000'
+      '/api':'http://localhost:${port}'
     }
   },
   plugins: [tailwindcss(),
