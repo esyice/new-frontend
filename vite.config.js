4ce1800d4e74b710b79944bd83b531b589eb63df
esyice <easydryice@gmail.com>
2025-03-23 03:58:06 +0530

    frontend env file added

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
 // https://vite.dev/config/
-export default defineConfig({
-  server:{
-    proxy:{
-      '/api':'http://localhost:${port}'
-    }
-  },
-  plugins: [tailwindcss(),
+export default defineConfig(({ mode }) => {
+  // Load environment variables
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    server: {
+      proxy: {
+        '/api': env.VITE_API_URL
+      }
+    },
+    plugins: [tailwindcss(),
     react()],
+  }
 })
